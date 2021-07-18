c813926b58f5600954dcc49896dff44b041b4de1
xingHome <xingqq2233@qq.com>
2021-07-18 21:31:15 +0800

    修改：删除临时文件

--- modules/time.js
+++
@@ -1,8 +0,0 @@
-const dateformat = require("dateformat");
-Date.now();
-let nowTime = () => dateformat(Date.now(), "yyyy-mm-dd HH:MM:ss");
-console.log(nowTime());
-console.log(nowTime());
-console.log(nowTime());
-console.log(Date.now());
-setInterval(() => console.log(nowTime()),1000 )
